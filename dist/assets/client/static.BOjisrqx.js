e1153e19414c8f025b0bfca8f0d6082936e5f39f
Alexis Sanehisa <asanehisa@yext.com>
2024-02-12 18:37:22 -0500

    fix

---
+++ dist/assets/client/static.BOjisrqx.js
@@ -0,0 +1,1 @@
+import{j as t}from"../static/index-DusxfY4x.js";import"../static/index-qrORHJ9L.js";const a=()=>t.jsx(t.Fragment,{children:"hello"});export{a as default};
---
+++ src/templates/static.tsx
@@ -0,0 +1,34 @@
+/**
+ * This is an example of how to create a static template that uses getStaticProps to retrieve data.
+ */
+import "../index.css";
+import {
+  Template,
+  GetPath,
+  TemplateProps,
+  TemplateRenderProps,
+} from "@yext/pages";
+
+/**
+ * Defines the path that the generated file will live at for production.
+ *
+ * NOTE: This currently has no impact on the local dev path. Local dev urls currently
+ * take on the form: featureName/entityId
+ */
+export const getPath: GetPath<TemplateProps> = () => {
+  return `index.html`;
+};
+
+/**
+ * This is the main template. It can have any name as long as it's the default export.
+ * The props passed in here are the direct result from `transformProps`.
+ */
+const Static: Template<TemplateRenderProps> = () => {
+  return (
+    <>
+      hello
+    </>
+  );
+};
+
+export default Static;
