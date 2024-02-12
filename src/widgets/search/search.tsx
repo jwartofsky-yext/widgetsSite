6fbc4133b00dc33b3db0cc87df72073bca2cc3ea
Alexis Sanehisa <asanehisa@yext.com>
2024-02-12 14:11:24 -0500

    add a bit more to widget

--- src/widgets/search/search.tsx
+++ src/widgets/search/search.tsx
@@ -1,4 +1,14 @@
-const search = () => {
+import { Widget, WidgetConfig, WidgetModule } from "@yext/pages/*";
+
+export const config: WidgetConfig = {
+  name: "search"
+}
+
+export const getPath = () => {
+  return "search";
+}
+
+const Search: Widget = () => {
   return(
     <div>
       search?
@@ -6,4 +16,9 @@ const search = () => {
   )
 }
 
-export default search;
+export const module: WidgetModule = {
+  config: config, 
+  default: Search
+}
+
+export default Search;
