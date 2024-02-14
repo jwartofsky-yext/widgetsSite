7a6857677945b4e02fc9a40f574138dea7664103
Alexis Sanehisa <asanehisa@yext.com>
2024-02-14 16:33:17 -0500

    add complex ish widget

--- src/widgets/complexWidget/complexWidget.tsx
+++ src/widgets/complexWidget/complexWidget.tsx
@@ -2,17 +2,42 @@ import { Widget, WidgetConfig, WidgetModule } from "@yext/pages/*";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+// this doesn't work rn
 export const config: WidgetConfig = {
-  name: "search"
+  name: "complexWidget"
 }
 
 const Search: Widget = () => {
   console.log("hello")
   return(
     <div>
-      search?
+      <App/>
     </div>
   )
+}
+
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      greeting: 'Hello, World!'
+    };
+  }
+
+  changeGreeting = () => {
+    this.setState({
+      greeting: 'Hello, React!'
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.greeting}</h1>
+        <button onClick={this.changeGreeting}>Change Greeting</button>
+      </div>
+    );
+  }
 }
 
 export const module: WidgetModule = {
---
+++ src/widgets/simpleWidget/simpleWidget.tsx
@@ -0,0 +1,36 @@
+import { Widget, WidgetConfig, WidgetModule } from "@yext/pages/*";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export const config: WidgetConfig = {
+  name: "simpleWidget"
+}
+
+const Search: Widget = () => {
+  console.log("hello")
+  return(
+    <div>
+      search?
+    </div>
+  )
+}
+
+export const module: WidgetModule = {
+  config: config, 
+  default: Search
+}
+if (typeof window !== 'undefined') {
+  const searchContainer = document.getElementById("searchContainer");
+  if (!searchContainer) {
+    throw new Error('could not find "searchContainer" element');
+  }
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <Search/>
+    </React.StrictMode>,
+    searchContainer
+  );
+}
+
+export default Search;
