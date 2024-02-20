a150f183b76da38a3080d9555c2ee5c3ac5a3bf6
Alexis Sanehisa <asanehisa@yext.com>
2024-02-20 10:25:19 -0500

    modules was last one

--- src/common/searchApp.tsx
+++ src/common/searchApp.tsx
@@ -1,6 +1,7 @@
-import { Pagination, ResultsCount, SearchBar, SpellCheck, StandardCard, VerticalConfigMap, VerticalResults } from '@yext/search-ui-react';
+import { provideHeadless, SearchHeadlessProvider } from '@yext/search-headless-react';
+import { SearchBar, UniversalResults, VerticalConfigMap } from '@yext/search-ui-react';
 import { v4 as uuidv4 } from 'uuid';
-import { SearchHeadlessProvider, provideHeadless } from '@yext/search-headless-react';
+import '@yext/search-ui-react/bundle.css'
 
 const config = {
   apiKey: 'ceedc10c919e565fd610f6240736ea81',
@@ -19,7 +20,6 @@ if (!sessionId) {
 }
 searcher.setSessionId(sessionId ?? 'sessionId');
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const verticalConfigMap: VerticalConfigMap = {
   help_articles: {
     label: "Help Articles"
@@ -29,14 +29,8 @@ const verticalConfigMap: VerticalConfigMap = {
 function App() {
   return (
     <SearchHeadlessProvider searcher={searcher}>
-        <SearchBar placeholder='Search...'/>
-        <SpellCheck />
-        <ResultsCount />
-        <VerticalResults
-          CardComponent={StandardCard}
-          displayAllOnNoResults={true}
-        />
-      <Pagination />
+      <SearchBar />
+      <UniversalResults verticalConfigMap={verticalConfigMap}/>
     </SearchHeadlessProvider>
   );
 }
